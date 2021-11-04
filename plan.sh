e1d17d8706ed3751cbefef19fc15d572e365bc48
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-04 01:54:42 +0000

    update to zulu 17.0.1

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_origin="ryanwoodsmall"
 pkg_name="zulu17musl"
 pkg_maintainer="ryanwoodsmall <rwoodsmall@gmail.com>"
-ver1="17.0.0"
-ver2="17.28.13"
+ver1="17.0.1"
+ver2="17.30.15"
 pkg_version="${ver1}.${ver2}"
 pkg_dirname="zulu${ver2}-ca-jdk${ver1}-linux_musl_x64"
 pkg_filename="${pkg_dirname}.tar.gz"
 pkg_source="https://cdn.azul.com/zulu/bin/${pkg_filename}"
-pkg_shasum="4de9984e98e6b41d494d2e41b73dbbfc6696f1a5ab83058d569f4e9c3832165b"
+pkg_shasum="02764bb49dc7e1a01bfa3b99ba7cc20ca8403dc41e914d1a5ae43a32eb25f7b1"
 pkg_license=( "GPL-2.0-only WITH Classpath-exception-2.0" )
 pkg_description=( 'Zulu is a build of the Open Java Development Kit (OpenJDK) on musl libc with long-term support from Azul' )
 pkg_upstream_url="https://www.azul.com/downloads/zulu-community/"
